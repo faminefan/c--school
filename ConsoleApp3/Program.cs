b04dd025a1acba8ef568d73bc515595920868b84
faminefan <andriytkahuk86@gmail.com>
2024-09-17 09:48:26 +0300

    14314

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("andriy");
+string nickname = Console.ReadLine();
+Console.WriteLine($"Hello, {nickname}!");
+int a = int.Parse (Console.ReadLine());
+Console.WriteLine($"degrees fahrenheit are {a * 1.8 + 32}");
