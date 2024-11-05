65d06c13337f6cbb933ebb563ca41d08bcd98646
faminefan <andriytkachuk86@gmail.com>
2024-11-05 10:11:27 +0200

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,124 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Enter your price: ");
-float a = float.Parse(Console.ReadLine());
+internal class Program
+{
+    static string direction = "Up";
 
-Console.WriteLine("Enter znushka : ");
-float b = float.Parse(Console.ReadLine());
+    static int tankX = 0;
+    static int tankY = 0;
 
+    public static void Main(string[] args)
+    {
+        Console.CursorVisible = false;
 
-decimal price = (decimal) (a - b * (a / 100));
-Console.WriteLine($"Trip price: {price}uah");
+        while (true)
+        {
+            Move();
+        }
+    }
+
+    static void Move()
+    {
+        var key = Console.ReadKey().Key;
+
+        switch (key)
+        {
+            case ConsoleKey.UpArrow:
+            case ConsoleKey.W:
+                ClearTank();
+
+                if (direction == "Up")
+                    tankY--;
+                else
+                    direction = "Up";
+
+                DrawTankUp();
+                break;
+            case ConsoleKey.DownArrow:
+            case ConsoleKey.S:
+                ClearTank();
+
+                if (direction == "Down")
+                    tankY++;
+                else
+                    direction = "Down";
+
+                DrawTankDown();
+                break;
+            case ConsoleKey.LeftArrow:
+            case ConsoleKey.A:
+                ClearTank();
+
+                if (direction == "Left")
+                    tankX -= 2;
+                else
+                    direction = "Left";
+
+                DrawLeft();
+                break;
+            case ConsoleKey.RightArrow:
+            case ConsoleKey.D:
+                ClearTank();
+
+                if (direction == "Right")
+                    tankX += 2;
+                else
+                    direction = "Right";
+
+                DrawRight();
+                break;
+        }
+    }
+
+    static void ClearTank()
+    {
+        Console.SetCursorPosition(tankX, tankY);
+        Console.Write("      ");
+        Console.SetCursorPosition(tankX, tankY + 1);
+        Console.Write("      ");
+        Console.SetCursorPosition(tankX, tankY + 2);
+        Console.Write("      ");
+    }
+    static void DrawTankUp()
+    {
+        Console.SetCursorPosition(tankX, tankY);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("  ██  ");
+        Console.SetCursorPosition(tankX, tankY + 1);
+        Console.Write("██████");
+        Console.SetCursorPosition(tankX, tankY + 2);
+        Console.Write("██  ██");
+        Console.ResetColor();
+    }
+    static void DrawTankDown()
+    {
+        Console.SetCursorPosition(tankX, tankY);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("██  ██");
+        Console.SetCursorPosition(tankX, tankY + 1);
+        Console.Write("██████");
+        Console.SetCursorPosition(tankX, tankY + 2);
+        Console.Write("  ██  ");
+        Console.ResetColor();
+    }
+    static void DrawLeft()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.SetCursorPosition(tankX, tankY);
+        Console.Write("  ████");
+        Console.SetCursorPosition(tankX, tankY + 1);
+        Console.Write("████  ");
+        Console.SetCursorPosition(tankX, tankY + 2);
+        Console.Write("  ████");
+        Console.ResetColor();
+    }
+    static void DrawRight()
+    {
+        Console.SetCursorPosition(tankX, tankY);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("████  ");
+        Console.SetCursorPosition(tankX, tankY + 1);
+        Console.Write("  ████");
+        Console.SetCursorPosition(tankX, tankY + 2);
+        Console.Write("████  ");
+        Console.ResetColor();
+    }
+}
